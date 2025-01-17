2294875ef4a62e523c4aacb9f60eced45451ea53
이준희 <ljh19980823@gmail.com>
2025-01-17 14:33:36 +0900

    [FEAT] 선물 전체 조회 Repository 코드 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/gift/repository/GiftRepository.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/gift/repository/GiftRepository.java
@@ -1,4 +1,17 @@
 package chocolate.chocoletter.api.gift.repository;
 
-public class GiftRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import chocolate.chocoletter.api.gift.domain.Gift;
+
+@Repository
+public interface GiftRepository extends JpaRepository<Gift, Long> {
+
+	@Query("select g from Gift g where g.receiverId = :receiverId")
+	List<Gift> findAll(@Param("receiverId") Long receiverId);
 }
