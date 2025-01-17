d151d2e6a92ad2c4bc94cf220794ed5b7f411fb5
이준희 <ljh19980823@gmail.com>
2025-01-17 16:10:26 +0900

    [FEAT] 전체 선물 리스트 조회 Controller 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/gift/controller/GiftController.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/gift/controller/GiftController.java
@@ -1,4 +1,25 @@
 package chocolate.chocoletter.api.gift.controller;
 
-public class GiftController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import chocolate.chocoletter.api.gift.dto.response.GiftsResponseDto;
+import chocolate.chocoletter.api.gift.service.GiftService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/gift")
+@RequiredArgsConstructor
+public class GiftController implements GiftSwagger {
+	private final GiftService giftService;
+
+	@GetMapping("/all")
+	public ResponseEntity<?> findAllGift() {
+		// 로그인 한 member를 가져오기
+		Long memberId = 1L;
+		GiftsResponseDto gifts = giftService.findAllGift(memberId);
+		return ResponseEntity.ok(gifts);
+	}
 }
