22ff579a8e1bb276ab648825153be5e60bac9706
이준희 <ljh19980823@gmail.com>
2025-01-17 16:04:56 +0900

    [FEAT] 전체 선물 조회 Service 코드 완성

--- chocoletter/src/main/java/chocolate/chocoletter/api/gift/service/GiftService.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/gift/service/GiftService.java
@@ -1,7 +1,13 @@
 package chocolate.chocoletter.api.gift.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
+import chocolate.chocoletter.api.gift.domain.Gift;
+import chocolate.chocoletter.api.gift.dto.response.GiftResponseDto;
+import chocolate.chocoletter.api.gift.dto.response.GiftsResponseDto;
 import chocolate.chocoletter.api.gift.repository.GiftRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -10,8 +16,10 @@ import lombok.RequiredArgsConstructor;
 public class GiftService {
 	private final GiftRepository giftRepository;
 
-	public void findAllGift(Long memberId) {
-		giftRepository.findAllGift(memberId);
-		return;
+	public GiftsResponseDto findAllGift(Long memberId) {
+		List<Gift> gifts = giftRepository.findAllGift(memberId);
+		return GiftsResponseDto.of(gifts.stream()
+			.map(GiftResponseDto::of)
+			.collect(Collectors.toList()));
 	}
 }
