3098d7165537f201e55cbcccda22018bf5db1056
Son-Hunseo <2423aa@naver.com>
2025-01-17 15:23:29 +0900

    [TEST] 배포 확인용 테스트 코드

--- chocoletter/src/main/java/chocolate/chocoletter/api/auth/controller/AuthController.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/auth/controller/AuthController.java
@@ -1,4 +1,16 @@
 package chocolate.chocoletter.api.auth.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController("/test")
 public class AuthController implements AuthSwagger{
+
+    @GetMapping
+    public ResponseEntity<?> test() {
+        return ResponseEntity.ok().body(new UserTest("son"));
+    }
 }
---
+++ chocoletter/src/main/java/chocolate/chocoletter/api/auth/controller/UserTest.java
@@ -0,0 +1,10 @@
+package chocolate.chocoletter.api.auth.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserTest {
+    private String name;
+}
