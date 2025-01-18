59e9db45099724116a9d2ef89dbc6a0a8f301453
김두철 <encjf2070@gmail.com>
2025-01-19 00:28:16 +0900

    🎨 [FEAT] login api 수정

--- chocoletter-frontend/src/pages/api/v1/auth/login.ts
+++ chocoletter-frontend/src/pages/api/v1/auth/login.ts
@@ -2,32 +2,24 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+  if (req.method === "GET") {
+    try {
+      // Spring 서버로 요청 전달
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/auth/kakao/login`
+      );
+      const { name, profile_url, is_first_login } = response.data;
 
-  if (!baseUrl) {
-    return res.status(500).json({
-      status: 500,
-      errorMessage: "ERR_INTERNAL_SERVER_ERROR",
-    });
-  }
+      // 클라이언트에 성공 응답
+      res.status(200).json({ name, profile_url, is_first_login });
+    } catch (error) {
+      // console.error("콘솔로 에러 찍어볼 수 있음", error);
 
-  try {
-    // Spring Boot로 GET 요청 전달
-    const response = await axios.get(`${baseUrl}/api/v1/users/auth/login`);
-
-    // 성공 응답 전달 (200)
-    res.status(200).json(response.data);
-  } catch (error: any) {
-    // console.error("에러 메세지 콘솔 찍어보기", error.message);
-
-    // 실패 응답 전달 (500)
-    if (error.response) {
-      res.status(500).json(error.response.data);
-    } else {
-      res.status(500).json({
-        status: 500,
-        errorMessage: "ERR_INTERNAL_SERVER_ERROR",
-      });
+      // 에러 메시지 통일
+      res.status(500).json({ status: 500, errorMessage: "ERR_INTERNAL_SERVER_ERROR" });
     }
+  } else {
+    // 허용되지 않은 메서드
+    res.status(500).json({ status: 500, errorMessage: "ERR_INTERNAL_SERVER_ERROR" });
   }
 }
