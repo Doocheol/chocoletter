d1c528133efc18fe4cc8a106dce06d1cbe36eed7
Jun2-Lee <ljh19980823@gmail.com>
2025-01-18 16:19:23 +0900

    [FEAT] 일반 선물 리스트 조회 Swagger 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/gift/controller/GiftSwagger.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/gift/controller/GiftSwagger.java
@@ -30,4 +30,14 @@ public interface GiftSwagger {
 			@ApiResponse(responseCode = "401", description = "인증 실패")
 	})
 	ResponseEntity<?> findSpecialGift();
+
+	@Operation(summary = "일반 선물 조회",
+			description = "로그인한 회원의 일반 선물을 조회합니다.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "성공적으로 조회",
+					content = @Content(mediaType = "application/json",
+							schema = @Schema(implementation = GiftsResponseDto.class))),
+			@ApiResponse(responseCode = "401", description = "인증 실패")
+	})
+	ResponseEntity<?> findGeneralGift();
 }
