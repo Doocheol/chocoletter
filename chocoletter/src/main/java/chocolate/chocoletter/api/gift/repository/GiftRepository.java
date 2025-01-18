ac642243aef1fc991112a1eb5d8175d7747cda6c
Jun2-Lee <ljh19980823@gmail.com>
2025-01-18 15:33:43 +0900

    [FEAT] 특별 선물 리스트 조회 컨트롤러 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/gift/repository/GiftRepository.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/gift/repository/GiftRepository.java
@@ -2,6 +2,7 @@ package chocolate.chocoletter.api.gift.repository;
 
 import java.util.List;
 
+import chocolate.chocoletter.api.gift.domain.GiftType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +15,7 @@ public interface GiftRepository extends JpaRepository<Gift, Long> {
 
 	@Query("select g from Gift g where g.receiverId = :receiverId")
 	List<Gift> findAllGift(@Param("receiverId") Long receiverId);
+
+	@Query("select g from Gift g where g.receiverId = :receiverId and g.type = :giftType")
+    List<Gift> findSpecialGift(@Param("receiverId") Long receiverId, @Param("giftType")GiftType giftType);
 }
