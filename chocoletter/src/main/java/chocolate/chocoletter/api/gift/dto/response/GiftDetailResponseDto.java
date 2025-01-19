9f6bea811fb3741a4cb08f11cc357a99b736c9c8
Jun2-Lee <ljh19980823@gmail.com>
2025-01-19 17:40:05 +0900

    [FEAT] 개별 선물 조회 ResponseDto  작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/gift/dto/response/GiftDetailResponseDto.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/gift/dto/response/GiftDetailResponseDto.java
@@ -3,18 +3,19 @@ package chocolate.chocoletter.api.gift.dto.response;
 import chocolate.chocoletter.api.gift.domain.Gift;
 import chocolate.chocoletter.api.letter.domain.Letter;
 import chocolate.chocoletter.api.letter.domain.LetterType;
+import chocolate.chocoletter.api.letter.dto.response.LetterDto;
 import lombok.Builder;
 
 @Builder
 public record GiftDetailResponseDto (Long giftId, LetterType type, String nickName, String content, String question, String answer){
-    public static GiftDetailResponseDto of(Gift gift, Letter letter) {
+    public static GiftDetailResponseDto of(Gift gift, LetterDto letter) {
         return GiftDetailResponseDto.builder()
                 .giftId(gift.getId())
-                .type(letter.getType())
-                .nickName(letter.getNickname())
-                .content(letter.getContent())
-                .question(letter.getQuestion())
-                .answer(letter.getAnswer())
+                .type(letter.type())
+                .nickName(letter.nickName())
+                .content(letter.content())
+                .question(letter.question())
+                .answer(letter.answer())
                 .build();
     }
 }
