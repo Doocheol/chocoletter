4e988919e1937c4e1059a7f18c0dbf3b013bcdcf
Jun2-Lee <ljh19980823@gmail.com>
2025-01-19 17:39:08 +0900

    [FEAT] 개별 선물 조회를 위한 Letter  서비스  작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/letter/service/LetterService.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/letter/service/LetterService.java
@@ -1,2 +1,17 @@
-package chocolate.chocoletter.api.letter.service;public class LetterService {
+package chocolate.chocoletter.api.letter.service;
+
+import chocolate.chocoletter.api.letter.dto.response.LetterDto;
+import chocolate.chocoletter.api.letter.domain.Letter;
+import chocolate.chocoletter.api.letter.repository.LetterRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LetterService {
+    private final LetterRepository letterRepository;
+    public LetterDto findLetter(Long giftId) {
+        Letter letter = letterRepository.findLetterByGiftId(giftId);
+        return LetterDto.of(letter);
+    }
 }
