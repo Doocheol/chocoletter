b6660d370cc33939cd2455cc150b6af5dc393071
Jun2-Lee <ljh19980823@gmail.com>
2025-01-20 09:39:02 +0900

    [CHORE] 코드 인덴트 맞추기

--- chocoletter/src/main/java/chocolate/chocoletter/api/gift/controller/GiftSwagger.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/gift/controller/GiftSwagger.java
@@ -22,22 +22,22 @@ public interface GiftSwagger {
 	ResponseEntity<?> findAllGift();
 
 	@Operation(summary = "특별 선물 조회",
-			description = "로그인한 회원의 특별 선물을 조회합니다.")
+		description = "로그인한 회원의 특별 선물을 조회합니다.")
 	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "성공적으로 조회",
-					content = @Content(mediaType = "application/json",
-							schema = @Schema(implementation = GiftsResponseDto.class))),
-			@ApiResponse(responseCode = "401", description = "인증 실패")
+		@ApiResponse(responseCode = "200", description = "성공적으로 조회",
+			content = @Content(mediaType = "application/json",
+				schema = @Schema(implementation = GiftsResponseDto.class))),
+		@ApiResponse(responseCode = "401", description = "인증 실패")
 	})
 	ResponseEntity<?> findSpecialGift();
 
 	@Operation(summary = "일반 선물 조회",
-			description = "로그인한 회원의 일반 선물을 조회합니다.")
+		description = "로그인한 회원의 일반 선물을 조회합니다.")
 	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "성공적으로 조회",
-					content = @Content(mediaType = "application/json",
-							schema = @Schema(implementation = GiftsResponseDto.class))),
-			@ApiResponse(responseCode = "401", description = "인증 실패")
+		@ApiResponse(responseCode = "200", description = "성공적으로 조회",
+			content = @Content(mediaType = "application/json",
+				schema = @Schema(implementation = GiftsResponseDto.class))),
+		@ApiResponse(responseCode = "401", description = "인증 실패")
 	})
 	ResponseEntity<?> findGeneralGift();
 }
