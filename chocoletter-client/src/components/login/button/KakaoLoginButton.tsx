dce36c938ffbc30e1d54e1b5ed93cff0a84cef6b
김두철 <encjf2070@gmail.com>
2025-01-20 17:18:42 +0900

    ✨ [FEAT] toast 추가 및 로그인 실패 로직 추가

--- chocoletter-client/src/components/login/button/KakaoLoginButton.tsx
+++ chocoletter-client/src/components/login/button/KakaoLoginButton.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import axios from "axios";
 import { useSetRecoilState } from "recoil";
-import { userNameAtom, userProfileUrlAtom } from "../../../atoms/auth/userAtoms";
+import {
+	userNameAtom,
+	userProfileUrlAtom,
+} from "../../../atoms/auth/userAtoms";
 import { ImageButton } from "../../common/ImageButton";
 import kakao_login_button from "../../../assets/images/button/kakao_login_button.png";
 
@@ -9,45 +12,50 @@ import kakao_login_button from "../../../assets/images/button/kakao_login_button
 import { toast } from "react-toastify";
 
 type KakaoLoginButtonProps = {
-  onLoginSuccess: (isFirstLogin: boolean) => void;
+	onLoginSuccess: (isFirstLogin: boolean) => void;
+	onLoginFailure?: (error: any) => void;
 };
 
-const KakaoLoginButton: React.FC<KakaoLoginButtonProps> = ({ onLoginSuccess }) => {
-  const setUserName = useSetRecoilState(userNameAtom);
-  const setUserProfileUrl = useSetRecoilState(userProfileUrlAtom);
+const KakaoLoginButton: React.FC<KakaoLoginButtonProps> = ({
+	onLoginSuccess,
+	onLoginFailure,
+}) => {
+	const setUserName = useSetRecoilState(userNameAtom);
+	const setUserProfileUrl = useSetRecoilState(userProfileUrlAtom);
 
-  const handleKakaoLogin = async () => {
-    try {
-      // const response = await axios.get("/api/v1/auth/login");
-      // const { name, profileUrl, isFirstLogin } = response.data;
+	const handleKakaoLogin = async () => {
+		try {
+			// const response = await axios.get("/api/v1/auth/login");
+			// const { name, profileUrl, isFirstLogin } = response.data;
 
-      //테스트 공간
-      const name = "김두철";
-      const profileUrl = "https://avatars.githubusercontent.com/u/77449569?v=4";
-      const isFirstLogin = true;
+			//테스트 공간
+			const name = "김두철";
+			const profileUrl =
+				"https://avatars.githubusercontent.com/u/77449569?v=4";
+			const isFirstLogin = true;
 
+			// Recoil 상태 업데이트
+			setUserName(name);
+			setUserProfileUrl(profileUrl);
 
-      // Recoil 상태 업데이트
-      setUserName(name);
-      setUserProfileUrl(profileUrl);
+			// 부모 컴포넌트로 첫 로그인 여부 전달
 
-      toast.success("로그인에 성공했습니다!");
+			onLoginSuccess(isFirstLogin);
+		} catch (error) {
+			// 로그인 실패 시 부모 컴포넌트로 에러 전달
+			if (onLoginFailure) {
+				onLoginFailure(error);
+			}
+		}
+	};
 
-      // 부모 컴포넌트로 첫 로그인 여부 전달
-
-      onLoginSuccess(isFirstLogin);
-    } catch (error) {
-      toast.error("로그인에 실패했습니다. 다시 시도해주세요.");
-    }
-  };
-
-  return (
-    <ImageButton
-      onClick={handleKakaoLogin}
-      backgroundImage={kakao_login_button}
-      className="w-48 h-12"
-    />
-  );
+	return (
+		<ImageButton
+			onClick={handleKakaoLogin}
+			backgroundImage={kakao_login_button}
+			className="w-48 h-12"
+		/>
+	);
 };
 
 export default KakaoLoginButton;
--- chocoletter-client/src/pages/LoginView.tsx
+++ chocoletter-client/src/pages/LoginView.tsx
@@ -6,30 +6,40 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 function LoginView() {
-  
+	const navigate = useNavigate();
 
-  const navigate = useNavigate();
+	const handleLoginSuccess = (isFirstLogin: boolean) => {
+		if (isFirstLogin) {
+			toast.info("첫 로그인입니다. 튜토리얼을 진행해주세요."); // 변경: info 메시지
+			// navigate("/tutorial-focus");
+		} else {
+			toast.success("로그인에 성공했습니다."); // 변경: success 메시지
+			// navigate("/main-my-before");
+		}
+	};
 
-  const handleLoginSuccess = (isFirstLogin: boolean) => {
-    if (isFirstLogin) {
-      toast.error("첫 로그인입니다. 튜토리얼을 진행해주세요.");
-      // navigate("/tutorial-focus");
-    } else {
-      toast.error("로그인에 성공했습니다.");
-      // navigate("/main-my-before");
-    }
-  };
+	const handleLoginFailure = (error: any) => {
+		const errorMessage =
+			error.response?.data?.message ||
+			"로그인에 실패했습니다. 다시 시도해주세요.";
+		toast.error(errorMessage);
+	};
 
-
-  return (
-    <div className="h-[calc((var(--vh, 1vh) * 100)-8rem)]">
-        <div className="h-1/3 flex justify-center items-center">
-          <img src={chocoletter_login_view_logo} alt="chocoletter_logo" className="max-h-40" />
-        </div>
-        <KakaoLoginButton onLoginSuccess={handleLoginSuccess}/>
-    </div>
-
-);
+	return (
+		<div className="h-[calc((var(--vh, 1vh) * 100)-8rem)]">
+			<div className="h-1/3 flex justify-center items-center">
+				<img
+					src={chocoletter_login_view_logo}
+					alt="chocoletter_logo"
+					className="max-h-40"
+				/>
+			</div>
+			<KakaoLoginButton
+				onLoginSuccess={handleLoginSuccess}
+				onLoginFailure={handleLoginFailure} // onLoginFailure 전달
+			/>
+		</div>
+	);
 }
 
 export default LoginView;
