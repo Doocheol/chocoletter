6e87cc856a35bd5818e8330a6db8b77034e5bd7c
이준희 <ljh19980823@gmail.com>
2025-01-20 13:01:56 +0900

    [FEAT] Qustion Entity 생성

---
+++ chocoletter/src/main/java/chocolate/chocoletter/api/letter/domain/Question.java
@@ -0,0 +1,14 @@
+package chocolate.chocoletter.api.letter.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Question {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String content;
+}
