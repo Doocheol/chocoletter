18c13295a058259a1918b0636344f55193f356a1
이준희 <ljh19980823@gmail.com>
2025-01-20 16:39:50 +0900

    [FEAT] RequestDto에 제약 조건 설명 Swagger 삭제(안나옴)

--- chocoletter/src/main/java/chocolate/chocoletter/api/letter/dto/request/RandomQuestionRequestDto.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/letter/dto/request/RandomQuestionRequestDto.java
@@ -1,17 +1,10 @@
 package chocolate.chocoletter.api.letter.dto.request;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record RandomQuestionRequestDto(
-	@Schema(
-		description = "이전 질문 ID (0 이상, 20 이하)",
-		example = "5",
-		minimum = "0",
-		maximum = "20"
-	)
 	@NotNull
 	@Min(0)
 	@Max(20)
