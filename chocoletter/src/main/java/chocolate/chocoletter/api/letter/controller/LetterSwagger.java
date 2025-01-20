d4e88af0e35d1b380bb2343211e2f66221f094e3
이준희 <ljh19980823@gmail.com>
2025-01-20 13:42:41 +0900

    [FEAT] 랜덤 질문 조회 Swagger 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/letter/controller/LetterSwagger.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/letter/controller/LetterSwagger.java
@@ -1,4 +1,28 @@
 package chocolate.chocoletter.api.letter.controller;
 
+import org.springframework.http.ResponseEntity;
+
+import chocolate.chocoletter.api.letter.dto.response.RandomQuestionResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 public interface LetterSwagger {
+	@Operation(
+		summary = "랜덤 질문 조회",
+		description = "랜덤 편지를 골랐을 때나 질문이 마음에 들지 않을 때, 질문을 조회합니다.")
+	@ApiResponses(
+		value = {@ApiResponse(
+			responseCode = "200",
+			description = "성공적으로 조회",
+			content = @Content(
+				mediaType = "application/json",
+				schema = @Schema(implementation = RandomQuestionResponseDto.class))),
+			@ApiResponse(
+				responseCode = "401",
+				description = "인증 실패")
+		})
+	ResponseEntity<?> findRandomQuestion(Long questionId);
 }
