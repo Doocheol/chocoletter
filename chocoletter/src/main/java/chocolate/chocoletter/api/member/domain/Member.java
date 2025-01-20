9984c32fb36871167cb55e8cd7167210f8a93d8b
이준희 <ljh19980823@gmail.com>
2025-01-20 15:28:37 +0900

    [CHORE] Member Entity Validation 제거

--- chocoletter/src/main/java/chocolate/chocoletter/api/member/domain/Member.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/member/domain/Member.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,13 +19,10 @@ public class Member extends BaseTimeEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotNull
 	private String name;
 
-	@NotNull
 	private String socialId;
 
-	@NotNull
 	private String alarmAddress;
 	private Integer sendGiftCount;
 
