040650f90f1e9ad0912121cb6957cbe8cecb2b17
이준희 <ljh19980823@gmail.com>
2025-01-20 16:53:37 +0900

    [FEAT] Question Entity DB 제약조건 설정

--- chocoletter/src/main/java/chocolate/chocoletter/api/letter/domain/Question.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/letter/domain/Question.java
@@ -1,5 +1,6 @@
 package chocolate.chocoletter.api.letter.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,7 +13,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Question {
 	@Id
+	@Column(unique = true, nullable = false)
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(nullable = false, unique = true)
 	private String content;
 }
