04c4dcf06f963c2ff082fa986bce91ca978de059
이준희 <ljh19980823@gmail.com>
2025-01-20 16:01:18 +0900

    [FEAT] ChatRoom Entity 제약조건 설정

--- chocoletter/src/main/java/chocolate/chocoletter/api/auth/controller/AuthController.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/auth/controller/AuthController.java
@@ -1,22 +1,21 @@
 package chocolate.chocoletter.api.auth.controller;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @RestController
 @RequestMapping("/test")
-public class AuthController implements AuthSwagger{
-    @Value("${DB_USERNAME}")
-    private String dbusername;
+public class AuthController implements AuthSwagger {
+	private String dbusername;
 
-    @GetMapping
-    public ResponseEntity<?> test() {
-        System.out.println(dbusername);
-        return ResponseEntity.ok().body(new UserTest("son"));
-    }
+	@GetMapping
+	public ResponseEntity<?> test() {
+		System.out.println(dbusername);
+		return ResponseEntity.ok().body(new UserTest("son"));
+	}
 }
