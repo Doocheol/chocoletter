d2b1001d68fa84b3c2f238dbc167d67409b7f857
이준희 <ljh19980823@gmail.com>
2025-01-20 13:09:18 +0900

    [FEAT] 랜덤 질문 조회 컨트롤러 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/letter/controller/LetterController.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/letter/controller/LetterController.java
@@ -1,4 +1,23 @@
 package chocolate.chocoletter.api.letter.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import chocolate.chocoletter.api.letter.service.LetterService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/letter")
+@RequiredArgsConstructor
 public class LetterController {
+	private final LetterService letterService;
+
+	@GetMapping("/question/{questionId}")
+	public ResponseEntity<?> findRandomQuestion(@PathVariable("questionId") Long questionId) {
+		letterService.findQuestion(questionId);
+		return ResponseEntity.ok("");
+	}
 }
