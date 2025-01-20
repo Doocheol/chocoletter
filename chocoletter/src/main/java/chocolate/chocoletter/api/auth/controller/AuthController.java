1d72be0d1497354c5f47023b539e80fadc58ead6
이준희 <ljh19980823@gmail.com>
2025-01-20 20:34:02 +0900

    [FEAT] AuthController 불필요한 코드 삭제

--- chocoletter/src/main/java/chocolate/chocoletter/api/auth/controller/AuthController.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/auth/controller/AuthController.java
@@ -5,17 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RestController
 @RequestMapping("/test")
 public class AuthController implements AuthSwagger {
-	private String dbusername;
-
-	@GetMapping
-	public ResponseEntity<?> test() {
-		System.out.println(dbusername);
-		return ResponseEntity.ok().body(new UserTest("son"));
-	}
 }
