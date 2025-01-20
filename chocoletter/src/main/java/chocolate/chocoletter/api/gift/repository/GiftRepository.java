fc7acc4be2bed3d0c4f466bbf25234b173e908d1
yeseul106 <yeseul106.dev@gmail.com>
2025-01-20 17:44:52 +0900

    #9 [ADD] giftId를 통해 gift 데이터 가져오는 repository 코드 구현

--- chocoletter/src/main/java/chocolate/chocoletter/api/gift/repository/GiftRepository.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/gift/repository/GiftRepository.java
@@ -1,8 +1,11 @@
 package chocolate.chocoletter.api.gift.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import chocolate.chocoletter.api.gift.domain.GiftType;
+import chocolate.chocoletter.common.exception.ErrorMessage;
+import chocolate.chocoletter.common.exception.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -21,4 +24,9 @@ public interface GiftRepository extends JpaRepository<Gift, Long> {
 
 	@Query("select g from Gift g where g.id = :giftId")
 	Gift findGiftById(@Param("giftId") Long giftId);
+
+	default Gift findGiftByIdOrThrow(Long giftId) {
+		return findById(giftId)
+				.orElseThrow(() -> new NotFoundException(ErrorMessage.ERR_NOT_FOUND_GIFT));
+	}
 }
