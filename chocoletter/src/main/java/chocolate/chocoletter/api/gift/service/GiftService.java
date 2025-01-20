ac3794557fdf62ec704fa33451a6c629c2204ae9
yeseul106 <20191037@sungshin.ac.kr>
2025-01-20 20:58:03 +0900

    #9 [CHORE] service 내부의 private 메서드 불필요한 static 선언 제거

--- chocoletter/src/main/java/chocolate/chocoletter/api/gift/service/GiftService.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/gift/service/GiftService.java
@@ -18,10 +18,19 @@ import chocolate.chocoletter.common.exception.UnAuthorizedException;
 import org.springframework.stereotype.Service;
 
 import chocolate.chocoletter.api.gift.domain.Gift;
+import chocolate.chocoletter.api.gift.domain.GiftType;
 import chocolate.chocoletter.api.gift.dto.response.GiftResponseDto;
+import chocolate.chocoletter.api.gift.dto.response.GiftUnboxingInvitationResponseDto;
 import chocolate.chocoletter.api.gift.dto.response.GiftsResponseDto;
 import chocolate.chocoletter.api.gift.repository.GiftRepository;
+import chocolate.chocoletter.common.exception.ErrorMessage;
+import chocolate.chocoletter.common.exception.ForbiddenException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -29,13 +38,19 @@ public class GiftService {
 	private final GiftRepository giftRepository;
 	private final LetterService letterService;
 
-	public GiftsResponseDto findAllGift(Long memberId) {
-		List<Gift> gifts = giftRepository.findAllGift(memberId);
-		return GiftsResponseDto.of(gifts.stream()
-			.map(GiftResponseDto::of)
-			.collect(Collectors.toList()));
-	}
+    public GiftsResponseDto findAllGift(Long memberId) {
+        List<Gift> gifts = giftRepository.findAllGift(memberId);
+        return GiftsResponseDto.of(gifts.stream()
+                .map(GiftResponseDto::of)
+                .collect(Collectors.toList()));
+    }
 
+    public GiftsResponseDto findSpecialGift(Long memberId) {
+        List<Gift> gifts = giftRepository.findSpecificGift(memberId, GiftType.SPECIAL);
+        return GiftsResponseDto.of(gifts.stream()
+                .map(GiftResponseDto::of)
+                .collect(Collectors.toList()));
+    }
 	public GiftsResponseDto findSpecialGift(Long memberId) {
 		List<Gift> gifts = giftRepository.findSpecificGift(memberId, GiftType.SPECIAL);
 		return GiftsResponseDto.of(gifts.stream()
@@ -57,24 +72,24 @@ public class GiftService {
 				.collect(Collectors.toList()));
 	}
 
-	public GiftsResponseDto findGeneralGift(Long memberId) {
-		List<Gift> gifts = giftRepository.findSpecificGift(memberId, GiftType.GENERAL);
-		return GiftsResponseDto.of(gifts.stream()
-				.map(GiftResponseDto::of)
-				.collect(Collectors.toList()));
-	}
+    public GiftsResponseDto findGeneralGift(Long memberId) {
+        List<Gift> gifts = giftRepository.findSpecificGift(memberId, GiftType.GENERAL);
+        return GiftsResponseDto.of(gifts.stream()
+                .map(GiftResponseDto::of)
+                .collect(Collectors.toList()));
+    }
 
-	public GiftUnboxingInvitationResponseDto findUnboxingInvitation(Long memberId, Long giftId) {
-		Gift gift = giftRepository.findGiftByIdOrThrow(giftId);
-		if (gift.getReceiverId().equals(memberId)) {
-			throw new ForbiddenException(ErrorMessage.ERR_FORBIDDEN);
-		}
-		String formattedUnboxingTime = formatUnboxingTime(gift.getUnBoxingTime());
-		return GiftUnboxingInvitationResponseDto.of(formattedUnboxingTime);
-	}
+    public GiftUnboxingInvitationResponseDto findUnboxingInvitation(Long memberId, Long giftId) {
+        Gift gift = giftRepository.findGiftByIdOrThrow(giftId);
+        if (gift.getReceiverId().equals(memberId)) {
+            throw new ForbiddenException(ErrorMessage.ERR_FORBIDDEN);
+        }
+        String formattedUnboxingTime = formatUnboxingTime(gift.getUnBoxingTime());
+        return GiftUnboxingInvitationResponseDto.of(formattedUnboxingTime);
+    }
 
-	private static String formatUnboxingTime(LocalDateTime unboxingTime) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm");
-		return unboxingTime.format(formatter);
-	}
+    private String formatUnboxingTime(LocalDateTime unboxingTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        return unboxingTime.format(formatter);
+    }
 }
