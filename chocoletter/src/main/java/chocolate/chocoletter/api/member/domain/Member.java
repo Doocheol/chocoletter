2143e0d76a1fce39043fb8750a97e22482a5a1ef
이준희 <ljh19980823@gmail.com>
2025-01-20 15:26:17 +0900

    [FEAT] Member Entity 생성자 추가

--- chocoletter/src/main/java/chocolate/chocoletter/api/member/domain/Member.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/member/domain/Member.java
@@ -5,7 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,12 +20,21 @@ public class Member extends BaseTimeEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotNull
 	private String name;
 
+	@NotNull
 	private String socialId;
 
+	@NotNull
 	private String alarmAddress;
-
 	private Integer sendGiftCount;
 
+	@Builder
+	public Member(String name, String socialId, String alarmAddress) {
+		this.name = name;
+		this.socialId = socialId;
+		this.alarmAddress = alarmAddress;
+		this.sendGiftCount = 0;
+	}
 }
