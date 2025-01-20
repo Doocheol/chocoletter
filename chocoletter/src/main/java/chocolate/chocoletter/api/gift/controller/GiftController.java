859afe7907324348fda1e462544c2bd3f040f48f
yeseul106 <20191037@sungshin.ac.kr>
2025-01-20 10:52:35 +0900

    #9 [ADD] Controller 계층 관련 코드 구현

--- chocoletter/src/main/java/chocolate/chocoletter/api/gift/controller/GiftController.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/gift/controller/GiftController.java
@@ -1,21 +1,22 @@
 package chocolate.chocoletter.api.gift.controller;
 
 import chocolate.chocoletter.api.gift.dto.response.GiftDetailResponseDto;
+import chocolate.chocoletter.api.gift.dto.response.GiftUnboxingInvitationResponseDto;
+import chocolate.chocoletter.api.gift.dto.response.GiftsResponseDto;
+import chocolate.chocoletter.api.gift.service.GiftService;
+import lombok.RequiredArgsConstructor;
+import chocolate.chocoletter.api.gift.dto.response.GiftUnboxingInvitationResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import chocolate.chocoletter.api.gift.dto.response.GiftsResponseDto;
-import chocolate.chocoletter.api.gift.service.GiftService;
-import lombok.RequiredArgsConstructor;
-
 @RestController
 @RequestMapping("/api/v1/gift")
 @RequiredArgsConstructor
 public class GiftController implements GiftSwagger {
-	private final GiftService giftService;
+    private final GiftService giftService;
 
 	@GetMapping("/all")
 	public ResponseEntity<?> findAllGifts() {
@@ -46,4 +47,12 @@ public class GiftController implements GiftSwagger {
 		GiftsResponseDto gifts = giftService.findGeneralGift(memberId);
 		return ResponseEntity.ok(gifts);
 	}
+
+    @Override
+    public ResponseEntity<?> findUnboxingInvitation() {
+        // 로그인 한 member를 가져오기
+        Long memberId = 1L;
+        GiftUnboxingInvitationResponseDto unboxingInvitation = giftService.findUnboxingInvitation(memberId);
+        return ResponseEntity.ok(unboxingInvitation);
+    }
 }
--- chocoletter/src/main/java/chocolate/chocoletter/api/gift/controller/GiftSwagger.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/gift/controller/GiftSwagger.java
@@ -1,5 +1,6 @@
 package chocolate.chocoletter.api.gift.controller;
 
+import chocolate.chocoletter.api.gift.dto.response.GiftUnboxingInvitationResponseDto;
 import chocolate.chocoletter.api.gift.dto.response.GiftDetailResponseDto;
 import org.springframework.http.ResponseEntity;
 
@@ -9,46 +10,86 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
 
 public interface GiftSwagger {
 
-	@Operation(summary = "전체 선물 목록 조회",
-		description = "로그인한 회원의 전체 선물 목록을 조회합니다.")
-	@ApiResponses(value = {
-		@ApiResponse(responseCode = "200", description = "성공적으로 조회",
-			content = @Content(mediaType = "application/json",
-				schema = @Schema(implementation = GiftsResponseDto.class))),
-		@ApiResponse(responseCode = "401", description = "인증 실패")
-	})
-	ResponseEntity<?> findAllGifts();
+    @Operation(
+            summary = "전체 선물 목록 조회",
+            description = "로그인한 회원의 전체 선물 목록을 조회합니다."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "성공적으로 조회",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = GiftsResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(responseCode = "401", description = "인증 실패")
+            }
+    )
+    ResponseEntity<?> findAllGifts();
 
-	@Operation(summary = "특별 선물 목록 조회",
-		description = "로그인한 회원의 특별 선물 목록을 조회합니다.")
-	@ApiResponses(value = {
-		@ApiResponse(responseCode = "200", description = "성공적으로 조회",
-			content = @Content(mediaType = "application/json",
-				schema = @Schema(implementation = GiftsResponseDto.class))),
-		@ApiResponse(responseCode = "401", description = "인증 실패")
-	})
-	ResponseEntity<?> findSpecialGifts();
+    @Operation(
+            summary = "특별 선물 목록 조회",
+            description = "로그인한 회원의 특별 선물 목록을 조회합니다."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "성공적으로 조회",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = GiftsResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(responseCode = "401", description = "인증 실패")
+            }
+    )
+    ResponseEntity<?> findSpecialGifts();
 
-	@Operation(summary = "개별 선물 조회",
-		description = "로그인한 회원이 개별 선물 내용을 조회합니다.")
-	@ApiResponses(value = {
-		@ApiResponse(responseCode = "200", description = "성공적으로 조회",
-			content = @Content(mediaType = "application/json",
-				schema = @Schema(implementation = GiftDetailResponseDto.class))),
-		@ApiResponse(responseCode = "401", description = "인증 실패")
-	})
-	ResponseEntity<?> findGiftDetail(Long giftId);
+    @Operation(summary = "개별 선물 조회",
+            description = "로그인한 회원이 개별 선물 내용을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 조회",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = GiftDetailResponseDto.class))),
+            @ApiResponse(responseCode = "401", description = "인증 실패")
+    })
+    ResponseEntity<?> findGiftDetail(Long giftId);
 
-	@Operation(summary = "일반 선물 목록 조회",
-		description = "로그인한 회원의 일반 선물 목록을 조회합니다.")
-	@ApiResponses(value = {
-		@ApiResponse(responseCode = "200", description = "성공적으로 조회",
-			content = @Content(mediaType = "application/json",
-				schema = @Schema(implementation = GiftsResponseDto.class))),
-		@ApiResponse(responseCode = "401", description = "인증 실패")
-	})
-	ResponseEntity<?> findGeneralGifts();
+    @Operation(summary = "일반 선물 목록 조회",
+            description = "로그인한 회원의 일반 선물 목록을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 조회",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = GiftsResponseDto.class))),
+            @ApiResponse(responseCode = "401", description = "인증 실패")
+    })
+    ResponseEntity<?> findGeneralGifts();
+
+    @Operation(
+            summary = "언박싱 초대장 조회",
+            description = "언박싱 초대장을 조회합니다."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "성공적으로 조회",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = GiftUnboxingInvitationResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "404", description = "해당하는 초대장이 없습니다.")
+            }
+    )
+    ResponseEntity<?> findUnboxingInvitation();
+
 }
