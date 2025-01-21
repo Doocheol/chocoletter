cb5e486060f0ac159b3cf54ba7118597d22c1348
이준희 <ljh19980823@gmail.com>
2025-01-21 16:55:49 +0900

    [FIX] GiftBoxController에 @Valid 추가

--- chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/controller/GiftBoxController.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/controller/GiftBoxController.java
@@ -4,6 +4,7 @@ import chocolate.chocoletter.api.giftbox.dto.request.GeneralFreeGiftRequestDto;
 import chocolate.chocoletter.api.giftbox.dto.request.GeneralQuestionRequestDto;
 import chocolate.chocoletter.api.giftbox.dto.request.SpecialFreeGiftRequestDto;
 import chocolate.chocoletter.api.giftbox.service.GiftBoxService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ public class GiftBoxController implements GiftBoxSwagger {
     private final GiftBoxService giftBoxService;
 
     @PostMapping("/{giftBoxId}/gift/general/free")
-    public ResponseEntity<?> sendGeneralFreeGift(@PathVariable("giftBoxId") Long giftBoxId, @RequestBody
+    public ResponseEntity<?> sendGeneralFreeGift(@PathVariable("giftBoxId") Long giftBoxId, @Valid @RequestBody
     GeneralFreeGiftRequestDto requestDto) {
         // 로그인 한 유저 찾아오기
         Long memberId = 1L;
@@ -29,7 +30,7 @@ public class GiftBoxController implements GiftBoxSwagger {
     }
 
     @PostMapping("/{giftBoxId}/gift/general/question")
-    public ResponseEntity<?> sendGeneralQuestionGift(@PathVariable("giftBoxId") Long giftBoxId, @RequestBody
+    public ResponseEntity<?> sendGeneralQuestionGift(@PathVariable("giftBoxId") Long giftBoxId, @Valid @RequestBody
     GeneralQuestionRequestDto requestDto) {
         // 로그인 한 유저 찾아오기
         Long memberId = 1L;
@@ -38,7 +39,7 @@ public class GiftBoxController implements GiftBoxSwagger {
     }
 
     @PostMapping("/{giftBoxId}/gift/special/free")
-    public ResponseEntity<?> sendSpecialFreeGift(@PathVariable("giftBoxId") Long giftBoxId, @RequestBody
+    public ResponseEntity<?> sendSpecialFreeGift(@PathVariable("giftBoxId") Long giftBoxId, @Valid @RequestBody
     SpecialFreeGiftRequestDto requestDto) {
         // 로그인 한 유저 찾아오기
         Long memberId = 1L;
