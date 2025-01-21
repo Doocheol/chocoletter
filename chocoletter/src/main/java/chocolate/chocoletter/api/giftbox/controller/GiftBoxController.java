14433a8d1ba03cef330356a346259f535476ec73
이준희 <ljh19980823@gmail.com>
2025-01-21 15:09:01 +0900

    [FEAT] 일반 질문 선물을 위한 Controller 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/controller/GiftBoxController.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/controller/GiftBoxController.java
@@ -1,5 +1,9 @@
 package chocolate.chocoletter.api.giftbox.controller;
 
+import chocolate.chocoletter.api.giftbox.dto.request.GeneralFreeGiftRequestDto;
+import chocolate.chocoletter.api.giftbox.dto.request.GeneralQuestionRequestDto;
+import chocolate.chocoletter.api.giftbox.service.GiftBoxService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,22 +12,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import chocolate.chocoletter.api.giftbox.dto.request.GeneralFreeGiftRequestDto;
-import chocolate.chocoletter.api.giftbox.service.GiftBoxService;
-import lombok.RequiredArgsConstructor;
-
 @RestController
 @RequestMapping("/api/v1/gift-box")
 @RequiredArgsConstructor
 public class GiftBoxController implements GiftBoxSwagger {
-	private final GiftBoxService giftBoxService;
+    private final GiftBoxService giftBoxService;
 
-	@PostMapping("/{giftBoxId}/gift/general/free")
-	public ResponseEntity<?> sendGeneralFreeGift(@PathVariable("giftBoxId") Long giftBoxId, @RequestBody
-	GeneralFreeGiftRequestDto requestDto) {
-		// 로그인 한 유저 찾아오기
-		Long memberId = 1L;
-		giftBoxService.sendGeneralFreeGift(memberId, giftBoxId, requestDto);
-		return ResponseEntity.status(HttpStatus.CREATED).build();
-	}
+    @PostMapping("/{giftBoxId}/gift/general/free")
+    public ResponseEntity<?> sendGeneralFreeGift(@PathVariable("giftBoxId") Long giftBoxId, @RequestBody
+    GeneralFreeGiftRequestDto requestDto) {
+        // 로그인 한 유저 찾아오기
+        Long memberId = 1L;
+        giftBoxService.sendGeneralFreeGift(memberId, giftBoxId, requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/{giftBoxId}/gift/general/question")
+    public ResponseEntity<?> sendGeneralQuestionGift(@PathVariable("giftBoxId") Long giftBoxId, @RequestBody
+    GeneralQuestionRequestDto requestDto) {
+        // 로그인 한 유저 찾아오기
+        Long memberId = 1L;
+        giftBoxService.sendGeneralQuestionGift(memberId, giftBoxId, requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
