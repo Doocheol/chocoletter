0ef32b51e78609b0bb22f0f2a80426d3e67f4df6
이준희 <ljh19980823@gmail.com>
2025-01-21 14:14:40 +0900

    [FEAT] 없는 giftBox로 보냈을 때, 예외처리

--- chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/service/GiftBoxService.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/service/GiftBoxService.java
@@ -9,6 +9,8 @@ import chocolate.chocoletter.api.giftbox.dto.request.GeneralFreeGiftRequestDto;
 import chocolate.chocoletter.api.giftbox.repository.GiftBoxRepository;
 import chocolate.chocoletter.api.letter.domain.Letter;
 import chocolate.chocoletter.api.letter.service.LetterService;
+import chocolate.chocoletter.common.exception.ErrorMessage;
+import chocolate.chocoletter.common.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -20,6 +22,8 @@ public class GiftBoxService {
 
 	public void sendGeneralFreeGift(Long senderId, Long giftBoxId, GeneralFreeGiftRequestDto requestDto) {
 		GiftBox receiverGiftBox = giftBoxRepository.findGiftBoxByGiftBoxId(giftBoxId);
+		if (receiverGiftBox == null)
+			throw new NotFoundException(ErrorMessage.ERR_NOT_FOUND);
 		Gift gift = Gift.createGeneralGift(receiverGiftBox, senderId, receiverGiftBox.getMember().getId());
 		giftService.saveGift(gift);
 		Letter letter = Letter.createGeneralLetter(gift, requestDto.nickName(), requestDto.Content());
