3a077662c4deab7b3bca59e1136e6bedc8f81149
Jun2-Lee <ljh19980823@gmail.com>
2025-01-21 19:56:42 +0900

    [FEAT] 내가 받은 선물 개수를 조회하는 Repository 코드 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/repository/GiftBoxRepository.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/repository/GiftBoxRepository.java
@@ -1,14 +1,16 @@
 package chocolate.chocoletter.api.giftbox.repository;
 
+import chocolate.chocoletter.api.giftbox.domain.GiftBox;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import chocolate.chocoletter.api.giftbox.domain.GiftBox;
-
 @Repository
 public interface GiftBoxRepository extends JpaRepository<GiftBox, Long> {
-	@Query("select gb from GiftBox gb join fetch gb.member where gb.id= :giftBoxId")
-	GiftBox findGiftBoxByGiftBoxId(@Param("giftBoxId") Long giftBoxId);
+    @Query("select gb from GiftBox gb join fetch gb.member where gb.id = :giftBoxId")
+    GiftBox findGiftBoxByGiftBoxId(@Param("giftBoxId") Long giftBoxId);
+
+    @Query("select gb.giftCount from GiftBox gb where gb.member.id = :memberId")
+    Integer findGiftCountByGiftBoxId(@Param("memberId") Long memberId);
 }
