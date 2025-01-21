c3033e8dab80035094a42bbcf255937d7e3488de
이준희 <ljh19980823@gmail.com>
2025-01-21 17:18:02 +0900

    [FEAT] 특별 질문 선물 보내기 Swagger 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/controller/GiftBoxSwagger.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/controller/GiftBoxSwagger.java
@@ -3,6 +3,7 @@ package chocolate.chocoletter.api.giftbox.controller;
 import chocolate.chocoletter.api.giftbox.dto.request.GeneralFreeGiftRequestDto;
 import chocolate.chocoletter.api.giftbox.dto.request.GeneralQuestionRequestDto;
 import chocolate.chocoletter.api.giftbox.dto.request.SpecialFreeGiftRequestDto;
+import chocolate.chocoletter.api.giftbox.dto.request.SpecialQuestionGiftRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -101,4 +102,32 @@ public interface GiftBoxSwagger {
             )
             @RequestBody
             SpecialFreeGiftRequestDto requestDto);
+
+    @Operation(
+            summary = "특별 질물 선물 전송",
+            description = "특정 GiftBox에 특별 질물 선물을 전송합니다. 로그인한 사용자의 ID가 필요합니다.",
+            tags = {"GiftBox"}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "선물이 성공적으로 전송되었습니다."
+            )
+    })
+    ResponseEntity<?> sendSpecialQuestionGift(
+            @Parameter(
+                    description = "선물을 전송할 GiftBox의 ID",
+                    required = true,
+                    example = "1"
+            )
+            @PathVariable("giftBoxId") Long giftBoxId,
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "특별 질문 선물의 상세 정보를 포함한 요청 본문",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = SpecialQuestionGiftRequestDto.class)
+                    )
+            ) @RequestBody
+            SpecialQuestionGiftRequestDto requestDto);
 }
