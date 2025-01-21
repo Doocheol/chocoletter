496d3a0710df69b9e5c27088d5604d45a6d80c4a
이준희 <ljh19980823@gmail.com>
2025-01-21 15:09:37 +0900

    [FEAT] 일반 질문 선물을 위한 Service 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/service/GiftBoxService.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/service/GiftBoxService.java
@@ -4,6 +4,7 @@ import chocolate.chocoletter.api.gift.domain.Gift;
 import chocolate.chocoletter.api.gift.service.GiftService;
 import chocolate.chocoletter.api.giftbox.domain.GiftBox;
 import chocolate.chocoletter.api.giftbox.dto.request.GeneralFreeGiftRequestDto;
+import chocolate.chocoletter.api.giftbox.dto.request.GeneralQuestionRequestDto;
 import chocolate.chocoletter.api.giftbox.repository.GiftBoxRepository;
 import chocolate.chocoletter.api.letter.domain.Letter;
 import chocolate.chocoletter.api.letter.service.LetterService;
@@ -22,12 +23,24 @@ public class GiftBoxService {
 
     public void sendGeneralFreeGift(Long senderId, Long giftBoxId, GeneralFreeGiftRequestDto requestDto) {
         GiftBox receiverGiftBox = giftBoxRepository.findGiftBoxByGiftBoxId(giftBoxId);
-		if (receiverGiftBox == null) {
-			throw new NotFoundException(ErrorMessage.ERR_NOT_FOUND);
-		}
+        if (receiverGiftBox == null) {
+            throw new NotFoundException(ErrorMessage.ERR_NOT_FOUND);
+        }
         Gift gift = Gift.createGeneralGift(receiverGiftBox, senderId, receiverGiftBox.getMember().getId());
         giftService.saveGift(gift);
         Letter letter = Letter.createGeneralLetter(gift, requestDto.nickName(), requestDto.Content());
+        letterService.saveLetter(letter);
+    }
+
+    public void sendGeneralQuestionGift(Long senderId, Long giftBoxId, GeneralQuestionRequestDto requestDto) {
+        GiftBox receiverGiftBox = giftBoxRepository.findGiftBoxByGiftBoxId(giftBoxId);
+        if (receiverGiftBox == null) {
+            throw new NotFoundException(ErrorMessage.ERR_NOT_FOUND);
+        }
+        Gift gift = Gift.createGeneralGift(receiverGiftBox, senderId, receiverGiftBox.getMember().getId());
+        giftService.saveGift(gift);
+        Letter letter = Letter.createQuestionLetter(gift, requestDto.nickName(), requestDto.question(),
+                requestDto.answer());
         letterService.saveLetter(letter);
     }
 }
