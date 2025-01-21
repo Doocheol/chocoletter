172b59e07ec846cccb04b58acc7a7e60cf37c63f
이준희 <ljh19980823@gmail.com>
2025-01-21 12:57:33 +0900

    [FEAT] 일반 자유 선물 보내기 api Controller 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/controller/GiftBoxController.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/controller/GiftBoxController.java
@@ -1,4 +1,27 @@
 package chocolate.chocoletter.api.giftbox.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import chocolate.chocoletter.api.gift.service.GiftService;
+import chocolate.chocoletter.api.giftbox.dto.request.GeneralFreeGiftRequestDto;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/gift-box")
+@RequiredArgsConstructor
 public class GiftBoxController {
+	private final GiftService giftService;
+
+	@PostMapping("/{giftBoxId}/gift/general/general")
+	public ResponseEntity<?> sendGeneralFreeGift(@PathVariable("giftBoxId") Long giftBoxId, @RequestBody
+	GeneralFreeGiftRequestDto requestDto) {
+		giftService.sendGeneralFreeGift(giftBoxId, requestDto);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
 }
