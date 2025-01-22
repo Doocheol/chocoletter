a7511253692fb2cd98e7006b642bfc95b3c35c2f
Jun2-Lee <ljh19980823@gmail.com>
2025-01-22 13:43:42 +0900

    [FEAT] 친구의 초콜릿 박스 조회 ResponseDto 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/dto/response/GiftBoxResponseDto.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/dto/response/GiftBoxResponseDto.java
@@ -1,4 +1,15 @@
 package chocolate.chocoletter.api.giftbox.dto.response;
 
-public class GiftBoxResponseDto {
+import chocolate.chocoletter.api.giftbox.domain.GiftBox;
+import lombok.Builder;
+
+@Builder
+public record GiftBoxResponseDto(String name, Long giftBoxId, Integer giftCount) {
+    public static GiftBoxResponseDto of(GiftBox giftBox) {
+        return GiftBoxResponseDto.builder()
+                .name(giftBox.getMember().getName())
+                .giftBoxId(giftBox.getId())
+                .giftCount(giftBox.getGiftCount())
+                .build();
+    }
 }
