df02571034b7f1224b82ef38faa705164cca88c1
김두철 <encjf2070@gmail.com>
2025-01-22 13:39:27 +0900

    ⚡️ [FEAT] Concurrent Mode (React 18 최신 기술) 설정

--- chocoletter-client/src/index.tsx
+++ chocoletter-client/src/index.tsx
@@ -2,14 +2,17 @@ import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import "./styles/index.css";
 import App from "./App";
+import React from "react";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 root.render(
-  <RecoilRoot>
-    <div className="mx-auto w-full md:max-w-sm lg:min-h-screen">
-      <App />
-    </div>
-  </RecoilRoot>
+	<React.StrictMode>
+		<RecoilRoot>
+			<div className="mx-auto w-full md:max-w-sm lg:min-h-screen">
+				<App />
+			</div>
+		</RecoilRoot>
+	</React.StrictMode>
 );
