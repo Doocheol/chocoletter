7978188e5cbfc361cb4aa1c7c3fd26de94946a67
김두철 <encjf2070@gmail.com>
2025-01-22 14:30:25 +0900

    🎨 [FEAT] Kakao JavaScript 타입 수정

--- chocoletter-client/src/types/kakao.d.ts
+++ chocoletter-client/src/types/kakao.d.ts
@@ -1,53 +1,53 @@
 // src/types/kakao.d.ts
+
 declare namespace Kakao {
     function init(appKey: string): void;
     function isInitialized(): boolean;
     
     const Share: {
-      sendDefault(options: Kakao.ShareOptions): void;
-      createDefaultButton(options: Kakao.ShareButtonOptions): void;
+        sendDefault(options: Kakao.ShareOptions): void;
+        createDefaultButton(options: Kakao.ShareButtonOptions): void;
     };
   
     interface ShareOptions {
-      objectType: "feed" | "list" | "commerce" | "location" | "text";
-      content: ShareContent;
-      social?: ShareSocial;
-      buttons?: ShareButton[];
+        objectType: "feed" | "list" | "commerce" | "location" | "text";
+        content: ShareContent;
+        social?: ShareSocial;
+        buttons?: ShareButton[];
     }
   
     interface ShareContent {
-      title: string;
-      description: string;
-      imageUrl: string;
-      link: ShareLink;
+        title: string;
+        description: string;
+        imageUrl: string;
+        link: ShareLink;
     }
   
     interface ShareLink {
-      mobileWebUrl: string;
-      webUrl: string;
+        mobileWebUrl: string;
+        webUrl: string;
     }
   
     interface ShareSocial {
-      likeCount?: number;
-      commentCount?: number;
-      sharedCount?: number;
+        likeCount?: number;
+        commentCount?: number;
+        sharedCount?: number;
     }
   
     interface ShareButton {
-      title: string;
-      link: ShareLink;
+        title: string;
+        link: ShareLink;
     }
   
     interface ShareButtonOptions {
-      container: string;
-      objectType: "feed" | "list" | "commerce" | "location" | "text";
-      content: ShareContent;
-      social?: ShareSocial;
-      buttons?: ShareButton[];
+        container: string;
+        objectType: "feed" | "list" | "commerce" | "location" | "text";
+        content: ShareContent;
+        social?: ShareSocial;
+        buttons?: ShareButton[];
     }
-  }
-  
-  interface Window {
+}
+
+interface Window {
     Kakao: typeof Kakao;
-  }
-  
+}
