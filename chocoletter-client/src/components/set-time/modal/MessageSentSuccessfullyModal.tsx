26cd3cce7cebe7419799124ff8dc0844dc5470c2
jisu <sjs7525@naver.com>
2025-01-23 13:15:12 +0900

    :sparkles: [CHORE] 안내 문구 및 버튼 위치 수정

--- chocoletter-client/src/components/set-time/modal/MessageSentSuccessfullyModal.tsx
+++ chocoletter-client/src/components/set-time/modal/MessageSentSuccessfullyModal.tsx
@@ -14,12 +14,16 @@ const MessageSentSuccessfullyModal: React.FC<MessageSentSuccessfullyProps> = ({ 
             isOpen={isOpen}
             onClose={onClose}
             className="w-[300px] h-[400px]"
-          >
-            카카오톡으로 상대방에게 편지를 함께 열어볼 시간을 물어봤어요! 💌
-            상대방이 시간을 수락하면 약속된 시간에 화상으로 연결됩니다.
-            만약 일정이 맞지 않다면, 최대 3번까지 시간을 재설정해서 다시 요청할 수 있어요.
-            발렌타인데이의 특별한 순간을 위해 조금만 더 기다려주세요! 🍫✨
-                <Button onClick={onClose}>내 초콜릿 상자로 이동하기</Button>
+            >
+                <h1 className="text-xl font-bold mb-4">
+                    카카오톡으로 상대방에게 편지를 함께 열어볼 시간을 물어봤어요! 💌
+                </h1>
+                <h3 className="text-sm mb-12 text-gray-500">
+                    상대방이 시간을 수락하면 약속된 시간에 화상으로 연결됩니다.
+                    만약 일정이 맞지 않다면, 최대 3번까지 시간을 재설정해서 다시 요청할 수 있어요.
+                    발렌타인데이의 특별한 순간을 위해 조금만 더 기다려주세요!✨
+                </h3>    
+                <Button onClick={onClose}>확인</Button>
             </Modal>
         </div>
     )
