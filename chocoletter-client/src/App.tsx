aad6631774fcfad415397e1ac1ac590d31eac933
김한송 <knhs4@naver.com>
2025-01-23 13:30:40 +0900

    🔧 [FIX] Openvidu 종료 시 subscribers 세션 처리 수정정

--- chocoletter-client/src/App.tsx
+++ chocoletter-client/src/App.tsx
@@ -17,6 +17,7 @@ import SentGiftView from "./pages/SentGiftView";
 import { ToastContainer } from "react-toastify";
 import MainMyBeforeView from "./pages/MainMyBeforeView";
 import ResetTimeView from "./pages/ResetTimeView";
+import RejectedView from "./pages/RejectedView";
 
 declare global {
 	interface Window {
@@ -64,6 +65,7 @@ function App() {
 					/>
 					<Route path="/video/room" element={<VideoRoomView />} />
 					<Route path="/reset-time" element={<ResetTimeView />} />
+					<Route path="/rejected" element={<RejectedView />} />
 				</Routes>
 			</BrowserRouter>
 			<ToastContainer
