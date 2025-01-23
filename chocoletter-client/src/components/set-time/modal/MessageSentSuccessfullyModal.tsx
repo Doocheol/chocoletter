e36a9cac6ae1c7d63f8c442ec768d6d25f913089
jisu <sjs7525@naver.com>
2025-01-23 13:09:26 +0900

    :sparkles: [Fix] 공통 모달 버튼 수정 - className 설정 가능 기능 추가

--- chocoletter-client/src/components/set-time/modal/MessageSentSuccessfullyModal.tsx
+++ chocoletter-client/src/components/set-time/modal/MessageSentSuccessfullyModal.tsx
@@ -10,11 +10,11 @@ interface MessageSentSuccessfullyProps {
 const MessageSentSuccessfullyModal: React.FC<MessageSentSuccessfullyProps> = ({ isOpen, onClose }) => {
     return (
         <div>
-            <Modal 
-            isOpen={isOpen} 
+            <Modal
+            isOpen={isOpen}
             onClose={onClose}
-            className="w-[250px] h-[200px]"
-            >
+            className="w-[300px] h-[400px]"
+          >
             카카오톡으로 상대방에게 편지를 함께 열어볼 시간을 물어봤어요! 💌
             상대방이 시간을 수락하면 약속된 시간에 화상으로 연결됩니다.
             만약 일정이 맞지 않다면, 최대 3번까지 시간을 재설정해서 다시 요청할 수 있어요.
