915556b6347f3f970c26b620c6b979d77bcce510
jisu <sjs7525@naver.com>
2025-01-23 14:39:21 +0900

    [CHORE] API 테스트

--- chocoletter-client/src/components/letter/Letter.tsx
+++ chocoletter-client/src/components/letter/Letter.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 
 type GiftProps = {
-    nickname: string;
+    nickName: string;
     content: string | null;
     question: string | null;
     answer: string | null;
 };
 
-const Gift: React.FC<GiftProps> = ({ nickname, content, question, answer }) => {
+const Gift: React.FC<GiftProps> = ({ nickName, content, question, answer }) => {
     return (
         <div className="flex flex-col justify-center items-center text-center">
             <h1 className="text-2xl font-bold mb-6">
-                {nickname} 님이 정성 가득 담아 <br />
+                {nickName} 님이 정성 가득 담아 <br />
                 보내주신 초콜렛이에요!❣️
             </h1>
 
             {/* 편지 내용 */}
-            <div className="flex flex-col justify-center items-center w-[300px] h-[600px] border-4 border-gray-300 bg-white p-4 rounded-lg">
+            <div className="flex flex-col justify-center items-center w-[300px] h-[600px] border-4 border-gray-300 bg-white p-2 rounded-lg">
                 {content ? (
                     // content가 있을 경우 content만 표시
                     <div className="flex justify-center items-center h-full">
@@ -27,7 +27,7 @@ const Gift: React.FC<GiftProps> = ({ nickname, content, question, answer }) => {
                     <div className="flex flex-col justify-between w-full h-full">
                         {/* question은 상단에 배치 */}
                         {question && (
-                            <p className="flex items-center justify-center h-[50px] text-lg bg-gray-200 rounded-lg">
+                            <p className="flex items-center justify-center h-[100px] text-lg bg-gray-200 rounded-lg">
                                 {question}
                             </p>
                         )}
--- chocoletter-client/src/pages/LetterView.tsx
+++ chocoletter-client/src/pages/LetterView.tsx
@@ -10,14 +10,14 @@ import Gift from "../components/letter/Letter";
 // 편지 보는 뷰
 // gift list page 에서 초콜릿 선택 시 보이게 됨.
 interface GiftData {
-    nickname?: string;
+    nickName?: string;
     content?: string | null;
     question?: string | null;
     answer?: string | null;
 }
 
 const LetterView = () => {
-    const giftId = 1 // useRecoilValue(selectedGiftIdAtom); //giftlist 페이지에서 저장된 giftId
+    const giftId = 2 // useRecoilValue(selectedGiftIdAtom); //giftlist 페이지에서 저장된 giftId
 
     const [giftData, setGiftData] = useState<GiftData | null>(null);
     const [loading, setLoading] = useState(true); 
@@ -57,7 +57,7 @@ const LetterView = () => {
                 <GiftView
                 giftData={
                     giftData || { 
-                        nickname: "Anonymous",
+                        nickName: "Anonymous",
                         content: null,
                         question: "No question provided",
                         answer: "No answer provided",
@@ -91,7 +91,7 @@ const LoadingView = () => (
 const GiftView: React.FC<{ giftData: GiftData }> = ({ giftData }) => (
     <div className="flex flex-col justify-center items-center mt-12">
         <Gift
-            nickname={giftData.nickname || "Anonymous"}
+            nickName={giftData.nickName || "Anonymous"}
             content={giftData.content || null}
             question={giftData.question || "No question provided"}
             answer={giftData.answer || "No answer provided"}
