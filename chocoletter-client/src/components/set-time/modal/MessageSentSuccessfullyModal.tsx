7dc56a959e9346ba0798f59a5d24521c80faa073
jisu <sjs7525@naver.com>
2025-01-23 13:02:27 +0900

    [FEAT] 화상 연결 일정 전송 완료 모달

---
+++ chocoletter-client/src/components/set-time/modal/MessageSentSuccessfullyModal.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Modal from "../../common/Modal";
+import { Button } from "../../common/Button";
+
+interface MessageSentSuccessfullyProps {
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+const MessageSentSuccessfullyModal: React.FC<MessageSentSuccessfullyProps> = ({ isOpen, onClose }) => {
+    return (
+        <div>
+            <Modal 
+            isOpen={isOpen} 
+            onClose={onClose}
+            className="w-[250px] h-[200px]"
+            >
+            카카오톡으로 상대방에게 편지를 함께 열어볼 시간을 물어봤어요! 💌
+            상대방이 시간을 수락하면 약속된 시간에 화상으로 연결됩니다.
+            만약 일정이 맞지 않다면, 최대 3번까지 시간을 재설정해서 다시 요청할 수 있어요.
+            발렌타인데이의 특별한 순간을 위해 조금만 더 기다려주세요! 🍫✨
+                <Button onClick={onClose}>내 초콜릿 상자로 이동하기</Button>
+            </Modal>
+        </div>
+    )
+}
+
+export default MessageSentSuccessfullyModal;
--- chocoletter-client/src/pages/SetTimeView.tsx
+++ chocoletter-client/src/pages/SetTimeView.tsx
@@ -1,12 +1,44 @@
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { Button } from "../components/common/Button";
 import { GoBackButton } from "../components/common/GoBackButton";
+import { GoArrowLeft } from "react-icons/go";
 import goBackIcon from "../assets/images/button/go_back_button.png";
+import MessageSentSuccessfullyModal from "../components/set-time/modal/MessageSentSuccessfullyModal";
 
 // 이 부분은 연습 페이지입니다. 기존 (혹은 추후) LetterViewrk 가 모두 대체할 예정이므로, 안 봐도 됩니다.
 const ExamplePage = () => {
+    const navigate = useNavigate();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+
+    // 초콜릿 만들기 버튼 누르면, 카카오톡 전송 완료 모달 띄우기
+    const sentGiftHandler = () => {
+        setIsModalOpen(true);
+    }
+
+    // 모달 닫기 + 페이지 이동
+    const closeModalAndNavigate = () => {
+        setIsModalOpen(false);
+        navigate("/sentgift"); // 원하는 경로로 이동
+    };
+
 	return (
-		<div className="relative h-full">
-			{/* <GoBackButton imageUrl={goBackIcon} altText="뒤로가기 버튼" /> */}
-			<h1 className="text-center">현재 페이지</h1>
+        <div className="relative flex flex-col items-center h-screen">
+            {/* 모달 컴포넌트 : 카카오톡 전송 완료 안내 & 편지 전송 완료 안내 화면으로 이동동 */}
+            <MessageSentSuccessfullyModal
+                    isOpen={isModalOpen}
+                    onClose={closeModalAndNavigate}
+            />
+            {/* 페이지 콘텐츠 */}
+            <GoBackButton icon={<GoArrowLeft />} altText="뒤로가기 버튼" />
+            <div className="absolute mt-24">
+                {/* <GoBackButton imageUrl={goBackIcon} altText="뒤로가기 버튼" /> */}
+                <h1 className="text-center">현재 페이지</h1>
+                <Button onClick={sentGiftHandler} className="py-5">
+                    초콜릿 개봉 초대장 보내기 📮
+                </Button>
+            </div>
 		</div>
 	);
 };
