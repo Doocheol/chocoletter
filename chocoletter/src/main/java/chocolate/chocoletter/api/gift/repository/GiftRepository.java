2ab4db067f93bcd0964389040abd0190d82cd48d
Jun2-Lee <ljh19980823@gmail.com>
2025-01-28 21:26:21 +0900

    #75 [FEAT] 선물을 주고 받을 때, 채팅방을 만들기 위해 GiftBox레포지토리 코드 추가

--- chocoletter/src/main/java/chocolate/chocoletter/api/gift/repository/GiftRepository.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/gift/repository/GiftRepository.java
@@ -28,8 +28,12 @@ public interface GiftRepository extends JpaRepository<Gift, Long> {
     @Query("select g from Gift g where g.senderId = :senderId and g.giftBox.id = :giftBoxId")
     Gift findGiftBySenderId(Long senderId, Long giftBoxId);
 
+    @Query("select g from Gift g where g.senderId = :senderId and g.receiverId = :receiverId and g.type = :giftType")
+    Gift findGeneralGiftBySenderIdAndReceiverId(Long senderId, Long receiverId, GiftType giftType);
+
     default Gift findGiftByIdOrThrow(Long giftId) {
         return findById(giftId)
                 .orElseThrow(() -> new NotFoundException(ErrorMessage.ERR_NOT_FOUND_GIFT));
     }
+
 }
