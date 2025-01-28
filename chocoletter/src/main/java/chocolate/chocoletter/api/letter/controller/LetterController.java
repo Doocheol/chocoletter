98a0666d14d8a54e00a9966d9b482cefb4083c64
Jun2-Lee <ljh19980823@gmail.com>
2025-01-29 01:44:49 +0900

    #79 [FEAT] Validation 추가

--- chocoletter/src/main/java/chocolate/chocoletter/api/letter/controller/LetterController.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/letter/controller/LetterController.java
@@ -2,6 +2,8 @@ package chocolate.chocoletter.api.letter.controller;
 
 import chocolate.chocoletter.api.letter.dto.response.RandomQuestionResponseDto;
 import chocolate.chocoletter.api.letter.service.LetterService;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +18,7 @@ public class LetterController implements LetterSwagger {
     private final LetterService letterService;
 
     @GetMapping("/question")
-    public ResponseEntity<?> findRandomQuestion(@RequestParam Long previousQuestionId) {
+    public ResponseEntity<?> findRandomQuestion(@RequestParam @Min(0) @Max(20) Long previousQuestionId) {
         RandomQuestionResponseDto randomQuestion = letterService.findRandomQuestion(previousQuestionId);
         return ResponseEntity.ok(randomQuestion);
     }
--- chocoletter/src/main/java/chocolate/chocoletter/api/letter/controller/LetterSwagger.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/letter/controller/LetterSwagger.java
@@ -27,7 +27,7 @@ public interface LetterSwagger {
             })
     ResponseEntity<?> findRandomQuestion(
             @Parameter(
-                    description = "이전 질문",
+                    description = "이전 질문 id, 처음 조회 시에는 해당 파라미터를 0으로 주고, 새로고침 시에는 이전 id를 주면 됩니다.",
                     required = true,
                     example = "1"
             ) @RequestParam Long previousQuestionId);
