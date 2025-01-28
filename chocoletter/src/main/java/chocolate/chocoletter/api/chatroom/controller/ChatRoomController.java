989a23cb0edbd44162b71edc5373180b5be954aa
Jun2-Lee <ljh19980823@gmail.com>
2025-01-29 00:13:13 +0900

    #78 [FEAT] 내 채팅방 리스트 조회 구현

--- chocoletter/src/main/java/chocolate/chocoletter/api/chatroom/controller/ChatRoomController.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/chatroom/controller/ChatRoomController.java
@@ -1,4 +1,25 @@
 package chocolate.chocoletter.api.chatroom.controller;
 
-public class ChatRoomController implements ChatRoomSwagger{
+import chocolate.chocoletter.api.chatroom.dto.response.ChatRoomsResponseDto;
+import chocolate.chocoletter.api.chatroom.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/chat-room")
+@RequiredArgsConstructor
+public class ChatRoomController implements ChatRoomSwagger {
+    private final ChatRoomService chatRoomService;
+
+    @GetMapping("/all")
+    public ResponseEntity<?> findMyChatRooms() {
+        // 로그인 한 멤버 찾아오기
+        Long memberId = 1L;
+        ChatRoomsResponseDto myChatRooms = chatRoomService.findMyChatRooms(memberId);
+        return ResponseEntity.ok(myChatRooms);
+    }
+
 }
--- chocoletter/src/main/java/chocolate/chocoletter/api/chatroom/controller/ChatRoomSwagger.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/chatroom/controller/ChatRoomSwagger.java
@@ -1,4 +1,28 @@
 package chocolate.chocoletter.api.chatroom.controller;
 
+import chocolate.chocoletter.api.chatroom.dto.response.ChatRoomsResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+
 public interface ChatRoomSwagger {
+
+    @Operation(
+            summary = "채팅방 조회",
+            description = "내가 속한 채팅방리스트를 조회합니다. 로그인한 사용자의 ID가 필요합니다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "조회 성공",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ChatRoomsResponseDto.class)
+                    )),
+            @ApiResponse(responseCode = "401", description = "인증되지 않았습니다.")
+    })
+    ResponseEntity<?> findMyChatRooms();
 }
