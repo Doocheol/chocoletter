d73f4093a34b345497ee52c5f078245e7cc545d7
Jun2-Lee <ljh19980823@gmail.com>
2025-01-29 00:17:52 +0900

    #79 [FIX] 랜덤 질문 조회 오류 수정

--- chocoletter/src/main/java/chocolate/chocoletter/api/letter/controller/LetterController.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/letter/controller/LetterController.java
@@ -1,26 +1,23 @@
 package chocolate.chocoletter.api.letter.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import chocolate.chocoletter.api.letter.dto.request.RandomQuestionRequestDto;
 import chocolate.chocoletter.api.letter.dto.response.RandomQuestionResponseDto;
 import chocolate.chocoletter.api.letter.service.LetterService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/letter")
 @RequiredArgsConstructor
 public class LetterController implements LetterSwagger {
-	private final LetterService letterService;
+    private final LetterService letterService;
 
-	@GetMapping("/question")
-	public ResponseEntity<?> findRandomQuestion(@Valid @RequestBody RandomQuestionRequestDto requestDto) {
-		RandomQuestionResponseDto randomQuestion = letterService.findRandomQuestion(requestDto.previousQuestionId());
-		return ResponseEntity.ok(randomQuestion);
-	}
+    @GetMapping("/question/{previousQuestionId}")
+    public ResponseEntity<?> findRandomQuestion(@PathVariable("previousQuestionId") Long previousQuestionId) {
+        RandomQuestionResponseDto randomQuestion = letterService.findRandomQuestion(previousQuestionId);
+        return ResponseEntity.ok(randomQuestion);
+    }
 }
