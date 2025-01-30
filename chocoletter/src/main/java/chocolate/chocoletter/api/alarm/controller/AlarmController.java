f6f79086bf3aae89b3d3390e5b3dc08e5be453d3
Jun2-Lee <ljh19980823@gmail.com>
2025-01-30 19:27:40 +0900

    #93 [FEAT] 전체 알람 조회를 위한 컨트롤러, Swagger 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/alarm/controller/AlarmController.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/alarm/controller/AlarmController.java
@@ -1,4 +1,24 @@
-package chocolate.chocoletter.api.alarm;
+package chocolate.chocoletter.api.alarm.controller;
 
-public class AlarmController {
+import chocolate.chocoletter.api.alarm.dto.response.AlarmsResponseDto;
+import chocolate.chocoletter.api.alarm.service.AlarmService;
+import java.security.Principal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/alarm")
+@RequiredArgsConstructor
+public class AlarmController implements AlarmSwagger {
+    private final AlarmService alarmService;
+
+    @GetMapping("/all")
+    public ResponseEntity<?> findMyAlarms(Principal principal) {
+        Long memberId = Long.parseLong(principal.getName());
+        AlarmsResponseDto myAlarms = alarmService.findMyAlarms(memberId);
+        return ResponseEntity.ok(myAlarms);
+    }
 }
