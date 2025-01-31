6ee7d315b7ebdb20e0f5469eaa10cff3430d95a5
Jun2-Lee <ljh19980823@gmail.com>
2025-01-31 12:49:47 +0900

    #99 [FEAT] 새 알림 조회 서비스 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/alarm/service/AlarmService.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/alarm/service/AlarmService.java
@@ -51,4 +51,8 @@ public class AlarmService {
                         row -> (String) row[1] // unboxingTime
                 ));
     }
+
+    public NewAlarmResponseDto findNewAlarms(Long memberId) {
+        return NewAlarmResponseDto.of(alarmRepository.countNewAlarm(memberId, false));
+    }
 }
