323559fe93435289af7ccd2fff6e8cddc6f05be4
김두철 <encjf2070@gmail.com>
2025-01-31 16:25:42 +0900

    🎨 [FEAT] 카카오 로그인 버튼 수정

--- chocoletter-client/src/components/main/my/before/button/KakaoShareButton.tsx
+++ chocoletter-client/src/components/main/my/before/button/KakaoShareButton.tsx
@@ -2,59 +2,58 @@ import React from "react";
 import { sendKakaoShare } from "../../../../../utils/sendKakaoTalk";
 import after_giftbox from "../../../../../assets/images/main/after_giftbox.svg";
 
-const KakaoShareButton: React.FC = () => {
-  const handleKakaoShare = () => {
-    const shareContent: Kakao.ShareOptions = {
-      objectType: "feed",
-      content: {
-        title: "초코레터",
-        description: "#초콜릿 #편지 #익명 #롤링페이퍼 #타임캡슐",
-        imageUrl: after_giftbox, // 이미지 URL 확인
-        link: {
-          mobileWebUrl: "https://yourdomain.com",
-          webUrl: "https://yourdomain.com",
-        },
-      },
-      social: {
-        likeCount: 777,
-        commentCount: 777,
-        sharedCount: 777,
-      },
-      buttons: [
-        {
-          title: "웹으로 보기",
-          link: {
-            mobileWebUrl: "https://yourdomain.com",
-            webUrl: "https://yourdomain.com",
-          },
-        },
-        {
-          title: "앱으로 보기",
-          link: {
-            mobileWebUrl: "https://yourdomain.com",
-            webUrl: "https://yourdomain.com",
-          },
-        },
-      ],
-    };
+interface KakaoShareButtonProps {
+	shareLink: string; // 부모(ShareModal)에서 받은 공유 링크
+}
 
-    sendKakaoShare(shareContent);
-  };
+const KakaoShareButton: React.FC<KakaoShareButtonProps> = ({ shareLink }) => {
+	const handleKakaoShare = () => {
+		// Kakao 링크에 사용될 옵션
+		const shareContent: Kakao.ShareOptions = {
+			objectType: "feed",
+			content: {
+				title: "초코레터",
+				description: "#초콜릿 #편지 #익명 #롤링페이퍼 #타임캡슐",
+				// (1) 이미지 URL -> afterGiftBox (임포트한 실제 경로가 빌드시 절대경로로 변환)
+				imageUrl: after_giftbox,
+				link: {
+					mobileWebUrl: shareLink, // (2) shareLink 사용
+					webUrl: shareLink,
+				},
+			},
+			social: {
+				likeCount: 777,
+				commentCount: 777,
+				sharedCount: 777,
+			},
+			buttons: [
+				{
+					title: "상대방 선물상자 바로가기",
+					link: {
+						mobileWebUrl: shareLink,
+						webUrl: shareLink,
+					},
+				},
+			],
+		};
 
-  return (
-    <button
-      onClick={handleKakaoShare}
-      // className="flex justify-center items-center bg-chocoletterBackground-light rounded-lg shadow hover:bg-sky-100 border border-black"
-      className="flex justify-center items-center border border-black rounded-xl transition focus:outline-none p-1 bg-yellow-300"
-      aria-label="카카오톡 공유"
-    >
-      <img
-        src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
-        alt="카카오톡 공유 보내기 버튼"
-        className="w-20 h-14 opacity-80"
-      />
-    </button>
-  );
+		// 실제 공유 호출
+		sendKakaoShare(shareContent);
+	};
+
+	return (
+		<button
+			onClick={handleKakaoShare}
+			className="flex justify-center items-center border border-black rounded-xl transition focus:outline-none p-1 bg-yellow-300"
+			aria-label="카카오톡 공유"
+		>
+			<img
+				src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
+				alt="카카오톡 공유 보내기 버튼"
+				className="w-20 h-14 opacity-80"
+			/>
+		</button>
+	);
 };
 
 export default KakaoShareButton;
