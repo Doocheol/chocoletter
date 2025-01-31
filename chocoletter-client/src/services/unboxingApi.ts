137269f99c3aa947f1c436c25319a0c9b8f019f3
jisu <sjs7525@naver.com>
2025-01-31 11:03:00 +0900

    [FEAT] 언박싱 스케줄 조회 및 불가능한 시간 막기

--- chocoletter-client/src/services/unboxingApi.ts
+++ chocoletter-client/src/services/unboxingApi.ts
@@ -1,6 +1,32 @@
 import api from "./api";
 
-// 언박싱 초대 시간 조회
+// 언박싱 전체 일정 조회(giftBoxId의 언박싱 전체 스케줄)
+// export async function getUnboxingSchedule(giftBoxId: number) {
+//   try {
+//     const res = await api.get(`/api/v1/gift-box/${giftBoxId}/unboxing/schedule`);
+//     return res.data;
+//   } catch (err) {
+//     console.error("getUnboxingSchedule API 호출 중 에러 발생:", err);
+//     return null;
+//   }
+// }
+
+const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxOCIsImlhdCI6MTczODI3OTk3MSwiZXhwIjoxNzM4ODg0NzcxfQ.i7E3fDn9tkwcJBQQCIy0y8Ev6dyfCICx79QBxJol4I0'
+export async function getUnboxingSchedule(giftBoxId: number) {
+  try {
+    const res = await api.get(`/api/v1/gift-box/${giftBoxId}/unboxing/schedule`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            }
+        })
+    return res.data;
+  } catch (err) {
+    console.error("getUnboxingSchedule API 호출 중 에러 발생:", err);
+    return null;
+  }
+}
+
+// 언박싱 초대 시간 조회(giftId의 언박싱 시간간)
 export async function getNotFixedUnboxingTime(giftId: number) {
   try {
     const res = await api.get(`/api/v1/gift/${giftId}/unboxing/invitation`);
@@ -33,13 +59,13 @@ export async function patchUnboxingReject(giftId: number) {
   }
 }
 
-// 언박싱 전체 일정 조회
-export async function getUnboxingSchedule(giftBoxId: number) {
+// 언박싱 일정 요청청
+export async function sendUnboxingTime(giftId: number) {
   try {
-    const res = await api.get(`/api/v1/gift-box/${giftBoxId}/unboxing/schedule`);
+    const res = await api.patch(`/api/v1/gift/${giftId}/unboxing/invitation/reject`);
     return res.data;
   } catch (err) {
-    console.error("getUnboxingSchedule API 호출 중 에러 발생:", err);
+    console.error("patchUnboxingReject API 호출 중 에러 발생:", err);
     return null;
   }
 }
