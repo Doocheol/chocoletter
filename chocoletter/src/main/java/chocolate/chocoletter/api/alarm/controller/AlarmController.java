5098a55e31a18a88ba19c4dffedb1f09af2784d9
Jun2-Lee <ljh19980823@gmail.com>
2025-01-31 12:50:13 +0900

    #99 [FEAT] 새 알림 조회 컨트롤러 & 스웨거 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/alarm/controller/AlarmController.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/alarm/controller/AlarmController.java
@@ -1,6 +1,7 @@
 package chocolate.chocoletter.api.alarm.controller;
 
 import chocolate.chocoletter.api.alarm.dto.response.AlarmsResponseDto;
+import chocolate.chocoletter.api.alarm.dto.response.NewAlarmResponseDto;
 import chocolate.chocoletter.api.alarm.service.AlarmService;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
@@ -20,5 +21,12 @@ public class AlarmController implements AlarmSwagger {
         Long memberId = Long.parseLong(principal.getName());
         AlarmsResponseDto myAlarms = alarmService.findMyAlarms(memberId);
         return ResponseEntity.ok(myAlarms);
+    }
+
+    @GetMapping("count")
+    public ResponseEntity<?> countMyAlarms(Principal principal) {
+        Long memberId = Long.parseLong(principal.getName());
+        NewAlarmResponseDto newAlarms = alarmService.findNewAlarms(memberId);
+        return ResponseEntity.ok(newAlarms);
     }
 }
