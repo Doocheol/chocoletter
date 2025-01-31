6af2bd66325bd804f27131bb0358492f73971b87
Jun2-Lee <ljh19980823@gmail.com>
2025-01-31 12:48:36 +0900

    #99 [FEAT] 새 알림 조회 레포지토리 작성

--- chocoletter/src/main/java/chocolate/chocoletter/api/alarm/repository/AlarmRepository.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/alarm/repository/AlarmRepository.java
@@ -4,10 +4,14 @@ import chocolate.chocoletter.api.alarm.domain.Alarm;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
     @Query("select a from Alarm a where a.memberId = :memberId order by a.createdAt desc")
-    List<Alarm> findByAlarms(Long memberId);
+    List<Alarm> findByAlarms(@Param("memberId") Long memberId);
+
+    @Query("select count(*) from Alarm a where a.memberId = :memberId and a.isRead = :isRead")
+    Integer countNewAlarm(@Param("memberId") Long memberId, @Param("isRead") Boolean isRead);
 }
