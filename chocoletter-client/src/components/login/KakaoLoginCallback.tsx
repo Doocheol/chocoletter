68c923876b9c3b0dfb83f2ded95c2d978208f857
김두철 <encjf2070@gmail.com>
2025-02-01 21:49:38 +0900

    🐛 [FIX] KakaoLoginCallback hotfix

--- chocoletter-client/src/components/login/KakaoLoginCallback.tsx
+++ chocoletter-client/src/components/login/KakaoLoginCallback.tsx
@@ -23,58 +23,58 @@ const KakaoLoginCallback: React.FC = () => {
 
   useEffect(() => {
     const handleLogin = async () => {
-      // URL의 쿼리 파라미터로 전달된 사용자 정보 추출
       const urlParams = new URLSearchParams(window.location.search);
       const accessToken = urlParams.get("accessToken");
       const userName = urlParams.get("userName");
       const userProfileUrl = urlParams.get("userProfileUrl");
       const isFirstLoginParam = urlParams.get("isFirstLogin");
-      const shareCode = urlParams.get("shareCode"); // 이제 shareCode를 받음
+      const shareCode = urlParams.get("shareCode");
 
       if (!accessToken || !userName || !shareCode) {
-        throw new Error("필수 로그인 정보가 누락되었습니다.");
         removeUserInfo();
-        toast.error("다시 로그인해주세요!");
+        toast.error("필수 로그인 정보가 누락되었습니다. 다시 로그인해주세요!");
+        navigate("/login");
+        return;
       }
 
       const isFirstLogin = isFirstLoginParam === "true";
       setIsFirstLogin(isFirstLogin);
 
-      // 사용자 정보 객체 생성
       const userInfo: MyUserInfo = {
         userName,
         accessToken,
         shareCode,
       };
 
-      // 사용자 정보 저장 (로컬 스토리지)
       saveUserInfo(userInfo);
 
-      // Recoil 상태 업데이트
       setIsLogin(true);
-      setUserName(userName); // 서버에서 실제 이름을 제공하는 경우, 해당 값을 사용
+      setUserName(userName);
       setUserProfileUrl(userProfileUrl || "");
       setShareCode(shareCode);
 
-      const location = useLocation() as { state: { redirect?: string } };
-      const redirectPath = location.state?.redirect;
+      const locationState = useLocation().state as { redirect?: string } | null;
+      const redirectPath = locationState?.redirect;
       if (redirectPath) {
         navigate(redirectPath);
+        return;
       }
 
-      // isFirstLogin이 true면, 먼저 /select-giftbox로 이동
       if (isFirstLogin) {
         navigate("/select-giftbox");
-      } else {
-        // 그 외에는 shareCode를 기반으로 메인 페이지로 이동
-        if (shareCode) {
-          navigate(`/${shareCode}`);
-        } else {
-          navigate("/");
-          removeUserInfo();
-          toast.error("다시 로그인해주세요!");
-        }
+        return;
       }
+
+      if (shareCode) {
+        navigate(`/${shareCode}`);
+        return;
+      } else {
+        removeUserInfo();
+        toast.error("다시 로그인해주세요!");
+        navigate("/");
+        return;
+      }
+      // 에러 발생 시 추가 처리 필요함
     };
 
     handleLogin();
