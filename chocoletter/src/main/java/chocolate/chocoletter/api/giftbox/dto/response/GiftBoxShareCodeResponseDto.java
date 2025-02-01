6c7147e7497e6167d373e9ec9c72c6e79ab1c525
son-hunseo <thsgnstj0512@gmail.com>
2025-02-02 00:22:36 +0900

    [FIX] shareCode 조회 api 수정

--- chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/dto/response/GiftBoxShareCodeResponseDto.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/dto/response/GiftBoxShareCodeResponseDto.java
@@ -1,7 +1,13 @@
 package chocolate.chocoletter.api.giftbox.dto.response;
 
+import chocolate.chocoletter.api.giftbox.domain.GiftBox;
+import lombok.Builder;
+
+@Builder
 public record GiftBoxShareCodeResponseDto(String shareCode) {
-    public static GiftBoxShareCodeResponseDto of(String shareCode) {
-        return new GiftBoxShareCodeResponseDto(shareCode);
+    public static GiftBoxShareCodeResponseDto of(GiftBox giftBox) {
+        return GiftBoxShareCodeResponseDto.builder()
+                .shareCode(giftBox.getShareCode())
+                .build();
     }
 }
