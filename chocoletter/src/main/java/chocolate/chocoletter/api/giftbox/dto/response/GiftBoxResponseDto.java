df7c4d7cc48da9b750c7e767dd1ea9d4fa8241c1
Hunseo Son <2423aa@naver.com>
2025-02-04 18:46:07 +0900

    [FIX] 친구 박스에 들어갔을 때 조회하는 api에 타입추가

--- chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/dto/response/GiftBoxResponseDto.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/giftbox/dto/response/GiftBoxResponseDto.java
@@ -4,11 +4,12 @@ import chocolate.chocoletter.api.giftbox.domain.GiftBox;
 import lombok.Builder;
 
 @Builder
-public record GiftBoxResponseDto(String name, String giftBoxId, Integer giftCount) {
+public record GiftBoxResponseDto(String name, String giftBoxId, Integer type, Integer giftCount) {
     public static GiftBoxResponseDto of(GiftBox giftBox, String encryptedGiftBoxId) {
         return GiftBoxResponseDto.builder()
                 .name(giftBox.getMember().getName())
                 .giftBoxId(encryptedGiftBoxId)
+                .type(giftBox.getType())
                 .giftCount(giftBox.getGiftCount())
                 .build();
     }
