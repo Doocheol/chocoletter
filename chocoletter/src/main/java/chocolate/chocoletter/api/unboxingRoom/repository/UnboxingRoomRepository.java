06cb1e4f134b90f885be5ae6474f9d8b1e826c0d
yeseul106 <20191037@sungshin.ac.kr>
2025-02-04 16:47:45 +0900

    #134 [ADD] gift id 리스트로 언박싱 룸 id 리스트 가져오는 쿼리 구현

--- chocoletter/src/main/java/chocolate/chocoletter/api/unboxingRoom/repository/UnboxingRoomRepository.java
+++ chocoletter/src/main/java/chocolate/chocoletter/api/unboxingRoom/repository/UnboxingRoomRepository.java
@@ -3,9 +3,11 @@ package chocolate.chocoletter.api.unboxingRoom.repository;
 import chocolate.chocoletter.api.unboxingRoom.domain.UnboxingRoom;
 import chocolate.chocoletter.common.exception.ErrorMessage;
 import chocolate.chocoletter.common.exception.NotFoundException;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -27,4 +29,7 @@ public interface UnboxingRoomRepository extends JpaRepository<UnboxingRoom, Long
 
     @Query("select r from UnboxingRoom r where r.id = :roomId")
     UnboxingRoom findUnboxingRoomByRoomId(Long roomId);
+
+    @Query("select r.gift.id, r.id from UnboxingRoom r where r.gift.id in :giftIds")
+    List<Object[]> findUnBoxingRoomIdsByGiftIds(@Param("giftIds") List<Long> giftIds);
 }
