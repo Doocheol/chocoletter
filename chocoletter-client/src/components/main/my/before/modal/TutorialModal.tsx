33cd3b422156540cbb1ef89c9718281433cc2856
jisu <sjs7525@naver.com>
2025-02-07 03:36:33 +0900

    [CHORE] 튜토리얼 수정

--- chocoletter-client/src/components/main/my/before/modal/TutorialModal.tsx
+++ chocoletter-client/src/components/main/my/before/modal/TutorialModal.tsx
@@ -12,8 +12,8 @@ const TutorialModal: React.FC<TutorialModalProps> = ({ isOpen, onClose }) => {
     "'초코레터'의 초콜릿은 특별합니다.",
     "- 익명으로 상대방에게 편지를 쓰고 초콜릿을 전달할 수 있어요.",
     "- 편지는 자유 양식과 랜덤 질문으로 구성되어 있어요.",
-    "- 2월 14일, 상대방과 함께 편지를 열기로 약속한 경우 둘만의 영상통화 공간이 열려요.",
-    " ",
+    "- 2월 14일, 상대방과 함께 편지를 열기로 약속한 경우",
+    "  둘만의 영상통화 공간이 열려요.",
     " ",
     "[2월 14일 이전]",
     "- 알림 아이콘을 클릭하여 영상 통화 수락/거절 여부를 선택해주세요.",
@@ -24,7 +24,7 @@ const TutorialModal: React.FC<TutorialModalProps> = ({ isOpen, onClose }) => {
     "[2월 14일 당일] Valentine's Day !",
     "- 선물상자를 오픈하면 모든 편지를 확인할 수 있어요.",
     "- 채팅 아이콘을 클릭하면 익명의 상대방과 대화할 수 있어요.",
-    "- 영상통화 가능한 초콜릿은 지정된 시간에 클릭하면 바로 통화가 연결됩니다.",
+    "- 영상통화 가능한 초콜릿은 지정된 시간에 클릭하면 통화가 연결됩니다.",
     "- 2월 14일에 접속하면 오늘 일정을 제공해드려요.",
     " ",
     " ",
@@ -35,7 +35,7 @@ const TutorialModal: React.FC<TutorialModalProps> = ({ isOpen, onClose }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} className="p-4">
       <div
-        className="flex flex-col px-4 py-2 tracking-tight leading-none text-left text-black bg-white rounded-3xl"
+        className="flex flex-col tracking-tight leading-none text-left text-black bg-white rounded-3xl"
         aria-labelledby="tutorial-modal-title"
       >
         <div className="flex flex-col justify-center items-center w-full">
