e5dd880c95ef6d5c5607a07db5bcdbf732aac847
김두철 <encjf2070@gmail.com>
2025-02-11 13:45:07 +0900

    🐛 [FIX] 로그인 수정

--- chocoletter-client/src/components/letter/modal/LetterEncryptedNoneModal.tsx
+++ chocoletter-client/src/components/letter/modal/LetterEncryptedNoneModal.tsx
@@ -1,21 +1,37 @@
+import { useNavigate } from "react-router";
 import { OtherModal } from "../../common/OtherModal";
 import { PurpleButton } from "../../common/PurpleButton";
 
 interface LetterEncryptedNoneModalProps {
-    isOpen: boolean;
+	isOpen: boolean;
+	onClose: () => void;
 }
 
-export const LetterEncryptedNoneModal: React.FC<LetterEncryptedNoneModalProps> = ({ isOpen }) => {
-    const onClose = () => {
-        // Do something
-    };
+const LetterEncryptedNoneModal: React.FC<LetterEncryptedNoneModalProps> = ({
+	isOpen,
+	onClose,
+}) => {
+	const navigate = useNavigate();
 
-    return(
-        <>
-            <OtherModal isOpen={isOpen} onClose={onClose} >
-                <p></p>
-                <PurpleButton />
-            </OtherModal>
-        </>
-    )
+	const handleConfirm = () => {
+		navigate("/");
+		onClose();
+	};
+
+	return (
+		<OtherModal isOpen={isOpen} onClose={handleConfirm}>
+			<div className="text-center flex flex-col justify-center items-center gap-6">
+				<div className="text-lg font-bold">브라우저 변경 알림</div>
+				<p className="text-sm text-gray-700">로그인을 다시 해주세요!</p>
+				<PurpleButton
+					onClick={handleConfirm}
+					className="px-6 py-3 text-white bg-chocoletterPurpleBold border border-black outline-none focus:ring-0"
+				>
+					확인
+				</PurpleButton>
+			</div>
+		</OtherModal>
+	);
 };
+
+export default LetterEncryptedNoneModal;
