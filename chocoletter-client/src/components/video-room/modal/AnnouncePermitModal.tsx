f0a12c2f8e17c9bb39b4a063635560cb580a56dc
김한송 <knhs4@naver.com>
2025-02-13 08:39:33 +0900

    [FIX] 화상채팅 5초전 안내 시간 및 허용 문구 수정

--- chocoletter-client/src/components/video-room/modal/AnnouncePermitModal.tsx
+++ chocoletter-client/src/components/video-room/modal/AnnouncePermitModal.tsx
@@ -8,6 +8,7 @@ export const AnnouncePermitModal: React.FC<AnnoouncePermitModalProps> = ({ video
             <div className="flex flex-col items-center justify-center text-center text-nowrap text-white">
                 <p className="mb-4">초코레터는 모든 음성과 영상을<br/><span className="text-chocoletterTextYellow">녹음, 녹화하지 않습니다!</span></p>
                 <p className="mb-4">영상통화를 위해<br/>이후 <span className="text-chocoletterGreen">Ready</span> 버튼을 누르면 나오는<br/>카메라와 비디오 <span className="text-chocoletterTextYellow">권한을 허용</span>해주세요!</p>
+                <p className="mb-4">추가로 원활한 영상통화를 위해<br/><span className="text-chocoletterTextYellow">현재 페이지를 벗어나거나 새로고침하지 말아주세요</span></p>
             </div>
             <div className="w-[11dvh] h-[11dvh] bottom-[10dvh] absolute bg-chocoletterGiftBg rounded-[100px] justify-center items-center gap-2.5 inline-flex z-20">
                 <button onClick={videoPermit} className="w-full h-full aspect-square flex justify-center items-center" >
