f02f47dfdfee477a05aa9363fc34fa8425383cb6
김한송 <knhs4@naver.com>
2025-02-15 17:39:05 +0900

    [CHORE] mainyourbeforeview 로그인 부분 주석 해제

--- chocoletter-client/src/pages/MainYourBeforeView.tsx
+++ chocoletter-client/src/pages/MainYourBeforeView.tsx
@@ -59,18 +59,18 @@ const MainYourBeforeView: React.FC = () => {
 	const dummyRef = useRef<HTMLDivElement>(null);
 
 	// 로그인 상태가 아니라면 바로 NotLoginModal만 렌더링
-	// if (!isLoggedIn) {
-	// 	return (
-	// 		<NotLoginModal
-	// 			isOpen={true}
-	// 			onClose={() => {}}
-	// 			onLogin={() => {
-	// 				localStorage.setItem("redirect", location.pathname);
-	// 				navigate("/");
-	// 			}}
-	// 		/>
-	// 	);
-	// }
+	if (!isLoggedIn) {
+		return (
+			<NotLoginModal
+				isOpen={true}
+				onClose={() => {}}
+				onLogin={() => {
+					localStorage.setItem("redirect", location.pathname);
+					navigate("/");
+				}}
+			/>
+		);
+	}
 
 	// 선물상자에 표시할 이름과 로딩 상태
 	const [recipientNickname, setRecipientNickname] = useState<string>("");
