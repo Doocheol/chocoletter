6f14180eebd36d4b779688f313706cffe9caf7c5
김두철 <encjf2070@gmail.com>
2025-02-15 18:24:27 +0900

    ✨ [FEAT] Sentry 설정 완료

--- chocoletter-client/vite.config.ts
+++ chocoletter-client/vite.config.ts
@@ -1,8 +1,16 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [react(), visualizer(), sentryVitePlugin({
+    org: "ssafy-05",
+    project: "javascript-react-5r"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
